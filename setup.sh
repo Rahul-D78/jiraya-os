ac49ce2e9f445cf6f475a0e29a428e3c183bb93d
Rahul-D78 <rahulparida933@gmail.com>
2022-01-10 22:24:07 +0530

    FEAT: script added for docker build
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
+apt update && apt upgrade -y
 apt-get install qemu -y && apt-get install docker.io -y
+sudo docker build . -t myos-buildenv
+
+os_name=${uname -s}
+
+if [os_name == Linux]
+then
+  docker run --rm -it -v $pwd:/root/env myos-buildenv
+else
+  echo "PLZ check how to do the same in your preferd OS ${os_name}"
+fi
